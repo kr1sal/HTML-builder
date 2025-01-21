6e1be8245d77ad454205744d9a7565171f5a9c77
kr1sal <kirillivanov523@gmail.com>
2025-01-21 11:28:01 +0300

    fix: don't output folders as files in the third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,15 +3,20 @@ const path = require('path');
 
 const folderpath = './03-files-in-folder/secret-folder';
 
-fs.readdir('./03-files-in-folder/secret-folder').then((files) => {
-  for (const file of files) {
-    const basename = path.basename(file, path.extname(file));
-    const extname = path.extname(file);
+async function main() {
+  const dir = await fs.opendir(folderpath);
+  for await (const dirent of dir) {
+    if (dirent.isFile()) {
+      const basename = path.basename(dirent.name, path.extname(dirent.name));
+      const extname = path.extname(dirent.name);
 
-    const filepath = path.join(folderpath, file);
-    fs.stat(filepath).then((stats) => {
-      const filesize = stats.size;
-      console.log(`${basename} - ${extname.slice(1)} - ${filesize}b`);
-    });
+      const filepath = path.join(folderpath, dirent.name);
+      fs.stat(filepath).then((stats) => {
+        const filesize = stats.size;
+        console.log(`${basename} - ${extname.slice(1)} - ${filesize}b`);
+      });
+    }
   }
-});
+}
+
+main();
