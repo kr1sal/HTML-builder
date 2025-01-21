5d51c57067bca14312e48281693b62c25c028dc2
kr1sal <kirillivanov523@gmail.com>
2025-01-21 11:17:57 +0300

    fix: double output in terminal,  raw mode is disabled for correct command processing and delete README.md in the task folder

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,20 +3,27 @@ const rl = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 
 const writeStream = fs.createWriteStream('./02-write-file/text.txt', 'utf-8');
-const rli = rl.createInterface({ input, output });
+const rli = rl.createInterface({ input, output, terminal: false });
+
+process.stdin.setRawMode(false);
 
 process.on('SIGINT', () => {
-  console.log('\nGoodbye!');
+  console.log();
   process.exit();
 });
 
 process.on('exit', (code) => {
   writeStream.end();
+  console.log('Goodbye!');
   console.log('Exited with status code:', code);
 });
 
 rli.on('line', (input) => {
-  writeStream.write(input + '\n');
+  if (input === 'exit') {
+    process.exit();
+  } else {
+    writeStream.write(input + '\n');
+  }
 });
 
 console.log('Welcome! Just enter text');
